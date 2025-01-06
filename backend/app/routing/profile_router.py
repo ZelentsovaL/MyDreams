dd5fe4d9960c18d2ab085d3d2f409aceca5fd7dc
xxxw1tnessbtwxxx <89967499+xxxw1tnessbtwxxx@users.noreply.github.com>
2025-01-06 17:59:17 +0700

    added profile router

--- backend/app/routing/profile_router.py
+++ backend/app/routing/profile_router.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from app.database.models.models import User
+from app.security.jwt_provider.jwtmanager import get_current_user
 
 profile_router = APIRouter(
     prefix="/profile",
@@ -6,5 +9,5 @@ profile_router = APIRouter(
 
 
 @profile_router.post("/update")
-async def update_profile():
-    ...
+async def update_profile(user: User = Depends(get_current_user)):
+    return user
