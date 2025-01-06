f5553ee08222a88a780b94cc7419a68f5d0eb6bb
xxxw1tnessbtwxxx <89967499+xxxw1tnessbtwxxx@users.noreply.github.com>
2025-01-06 17:58:57 +0700

    added including profille router

--- backend/app/routing/main_router.py
+++ backend/app/routing/main_router.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter
+
 from app.routing.access_router import access_router
+from app.routing.profile_router import profile_router
 
 main_router = APIRouter(
     prefix="/v1",
 )
 
 main_router.include_router(access_router)
+main_router.include_router(profile_router)
